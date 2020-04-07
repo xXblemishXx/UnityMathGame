f08c981b2893ad16371d0dbe8359fa169845fea1
Asage7 <mcfs@hotmail.com.br>
2020-04-07 11:38:48 -0300

    Seleção adicionada com troca de sprites

--- Assets/Scripts/OnClick.cs
+++ Assets/Scripts/OnClick.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class OnClick : MonoBehaviour
 {
-    void OnMouseDown()
+	public Sprite sprite1; 
+	public Sprite sprite2; 
+	public GameObject selectedObject;
+	public SpriteRenderer changeSpriteVar;
+	private void Update()
     {
-        Destroy(transform.parent.gameObject);
-    }
+	RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (hit && hit.collider.gameObject.transform.root.CompareTag("CanClick"))
+			{
+				if (selectedObject != null)
+				{
+					changeSpriteVar=selectedObject.GetComponentInChildren<SpriteRenderer>();
+					changeSpriteVar.sprite = sprite1;
+				}
+				selectedObject = hit.collider.gameObject.transform.root.gameObject;
+				changeSpriteVar = selectedObject.GetComponentInChildren<SpriteRenderer>();
+				changeSpriteVar.sprite = sprite2;
+			}
+		}
+	}
 }
