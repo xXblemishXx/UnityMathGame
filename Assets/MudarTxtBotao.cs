f0cca66a9ba217aacba250987fa3beab59cb3eb0
Blemish <39205389+xXblemishXx@users.noreply.github.com>
2020-04-10 20:38:40 -0300

    Botões de Respostas
    Adicionei os botões que por enquanto estão gerando numeros aleatórios entre 0 e 100 e ao clicar ele altera o valor

---
+++ Assets/MudarTxtBotao.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MudarTxtBotao : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = mudarValor();
+        //txt.text = "teste";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MudarValor()
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = mudarValor();
+    }
+    public string mudarValor()
+    {
+
+        int a = Random.Range(0, 100);
+        return a.ToString();
+    }
+}
